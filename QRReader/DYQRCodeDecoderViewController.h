f633ab00cea37fa4457d818f484659ef27b5662c
Muhammad Azher <muhammadazher@ozimacmini9s-Mac-mini.local>
2017-08-30 19:01:02 +0500

    QRCode Scanner Added

---
+++ QRReader/DYQRCodeDecoderViewController.h
@@ -0,0 +1,33 @@
+//
+//  DYQRCodeDecoderViewController.h
+//  QRCode-Decoder
+//
+//  Created by Dwarven on 16/7/5.
+//  Copyright © 2016 Dwarven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+
+@interface DYQRCodeDecoderViewController : UIViewController
+
+@property (nonatomic, strong, readonly) UIBarButtonItem * leftBarButtonItem;
+@property (nonatomic, strong, readonly) UIBarButtonItem * rightBarButtonItem;
+@property (nonatomic, strong) UIColor * navigationBarTintColor;
+@property (nonatomic, strong) UIImage * frameImage;
+@property (nonatomic, strong) UIImage * lineImage;
+@property (nonatomic) BOOL needsScanAnnimation;
+//@property (weak, nonatomic) IBOutlet UITextView *myResult;
+@property (nonatomic,strong) NSString *myResult;
+
+- (id)initWithCompletion:(void(^)(BOOL succeeded, NSString * result))completion;
+
+#pragma mark - methods for subclass
+
+- (void)start;
+- (void)stop;
+- (void)dealWithResult:(NSString *)result;
+- (void)cancel;
+
+@end
--- QRReader/ViewController.m
+++ QRReader/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "DYQRCodeDecoderViewController.h"
 
 @interface ViewController ()
 
@@ -16,14 +17,57 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.resultString.text = self.tmpResult;
+//    [self setNavigationBarButton];
     // Do any additional setup after loading the view, typically from a nib.
+ 
+    
+//    [self.view addSubview:vc.view];
+
+    
+    
 }
 
-
+-(void)setNavigationBarButton{
+    UIView *buttonContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
+    //    buttonContainer.backgroundColor = [UIColor clearColor];
+    UIButton *button0 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button0 setFrame:CGRectMake(40, 10, 100,25)];
+    [button0 setTitle:@"OFF" forState:UIControlStateNormal];
+    [button0.layer setCornerRadius:12.5];
+    [button0 setImage:[UIImage imageNamed:@"flash"] forState:UIControlStateNormal];
+    button0.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 10);
+    //    [button0 setBackgroundImage:[UIImage imageNamed:@"button0.png"] forState:UIControlStateNormal];
+    [button0 addTarget:self action:@selector(turnTorchOnOrOff:) forControlEvents:UIControlEventTouchUpInside];
+    //    [button0 setShowsTouchWhenHighlighted:YES];
+    [button0 setBackgroundColor:[UIColor blackColor]];
+    [buttonContainer addSubview:button0];
+    
+    
+    
+    UIButton *button1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button1 setFrame:CGRectMake((buttonContainer.frame.size.width)-150, 10, 100, 25)];
+    [button1.layer setCornerRadius:12.5];
+    [button1 setTitle:@"Library" forState:UIControlStateNormal];
+    [button1 addTarget:self action:@selector(openLibrary:) forControlEvents:UIControlEventTouchUpInside];
+    [button1 setBackgroundColor:[UIColor blackColor]];
+    [button1 setImage:[UIImage imageNamed:@"gallery"] forState:UIControlStateNormal];
+    button1.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 10);
+    [buttonContainer addSubview:button1];
+    
+    
+    self.navigationItem.titleView = buttonContainer;
+    self.navigationController.navigationBar.topItem.titleView = buttonContainer;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(IBAction)turnTorchOnOrOff:(id)sender{
+    NSLog(@"Flash is Turning on or off");
+}
+-(IBAction)openLibrary:(id)sender{
+    NSLog(@"Opening Library");
 
-
+}
 @end
